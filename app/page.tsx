401890d012395d9134418b27ade1322477209a85
Ali <ali.arioglu06@gmail.com>
2024-03-16 11:45:38 +0300

    buttons component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="text-green-500">hello lingo</div>;
+  return (
+    <div className="p-4 flex flex-col gap-4 max-w-[200px]">
+      <Button>Button</Button>
+      <Button variant="primary">Button</Button>
+      <Button variant="primaryOutline">Button</Button>
+      <Button variant="secondary">Button</Button>
+      <Button variant="secondaryOutline">Button</Button>
+    </div>
+  );
 }
