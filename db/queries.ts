48f9057391de8c49582e789ecf7105abac68a9bc
Ali <ali.arioglu06@gmail.com>
2024-03-17 12:43:52 +0300

    user progress

--- db/queries.ts
+++ db/queries.ts
@@ -1,9 +1,37 @@
 import { cache } from "react";
+import { eq } from "drizzle-orm";
+import { auth } from "@clerk/nextjs";
+
 import db from "./drizzle";
+import { courses, userProgress } from "./schema";
 
 // Her bu fonksiyonu çağırdığımda db isteği atmasın diye cacheledim.
+
+export const getUserProgress = cache(async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+
+  return data;
+});
+
 export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
+
+  return data;
+});
+
+export const getCourseById = cache(async (courseId: number) => {
+  const data = await db.query.courses.findFirst({
+    where: eq(courses.id, courseId),
+  });
 
   return data;
 });
